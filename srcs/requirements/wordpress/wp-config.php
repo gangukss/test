32491bff057d6cc6dec3566a59c4dcfcd69c2e7a
kankim <rkddnr7328@naver.com>
2021-11-28 20:44:07 +0900

    test inception

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', '${DB_USER}');
 define('DB_PASSWORD', '${DB_PASSWORD}');
 
 /** MySQL hostname */
-define('DB_HOST', 'mariadb');
+define('DB_HOST', '${DB_HOST}');
 
 /** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
